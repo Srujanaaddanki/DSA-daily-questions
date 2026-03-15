95251a5abe6cdc3d94ee7806229e9b94f6e817b9
Srujana Addanki <152511711+Srujanaaddanki@users.noreply.github.com>
2026-03-15 22:01:25 +0530

    Time: 7 ms (13.54%), Space: 46.1 MB (29.91%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isPalindrome(int x) { 
+        if (x < 0) {                     //All negative numbers are automatically FALSE
+            return false;
+        }      
+        String s = String.valueOf(x);   // 2. Convert the integer 'x' into a String 's'    
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }       
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         int digit;
+//         int rev=0;
+//         int original = x;
+//         while(x>0){
+//             digit=x%10;
+//             rev = (rev*10) + digit;
+//             x=x/10;
+//         }
+//         if (rev == original){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+// }
