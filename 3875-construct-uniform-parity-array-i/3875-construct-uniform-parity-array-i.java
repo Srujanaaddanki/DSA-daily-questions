ae273ace490478b86577df0a1b37e59aee927a64
Srujana Addanki <152511711+Srujanaaddanki@users.noreply.github.com>
2026-03-23 01:56:03 +0530

    Time: 0 ms (100%), Space: 45.2 MB (33.33%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,24 @@
+// class Solution {
+//     public boolean uniformArray(int[] nums1) {
+//         int n = nums1.length;
+//         int e =0;
+//         int o=0;
+//         for(int i=0;i<n;i++){
+//             if(nums1[i]%2 == 0)
+//                 e++;
+//             else
+//                o++;
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
