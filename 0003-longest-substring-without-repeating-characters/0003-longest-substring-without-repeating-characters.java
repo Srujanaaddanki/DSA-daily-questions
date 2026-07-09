5c4e7d9ce0b61bf61040ef8cf11ae186be9ca559
Srujana Addanki <152511711+Srujanaaddanki@users.noreply.github.com>
2026-07-09 22:10:19 +0530

    Enhance code with comments on logic differences
    Added comments explaining the core logic differences between if statements and while loops in the context of finding the longest substring without repeating characters.

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -15,3 +15,18 @@ class Solution {
         return maxLength;
     }
 }
+
+
+
+//The Difference in Core Logic   
+//if statement: Looks at the condition exactly once. If a duplicate exists, it shrinks the left side by exactly one step and moves on.
+//while loop: Keeps running repeatedly until the condition becomes completely false. It shrinks the left side step-by-step until the duplicate character is completely kicked out of the window.
+
+//we are using 0-based indexing.
+//If you use just right - left
+//your formula (right - left):   --> 0 - 0 = 0 
+//The math says you have 0 characters. But your eyes clearly see 1 character ("a") in the window! Your count is wrong.
+
+//Using the correct formula (right - left + 1):
+//0 - 0 + 1 = 1
+//The math correctly matches reality.
