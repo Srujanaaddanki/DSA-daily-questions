b7e6890be9e6c1e2fc7fcd4e02c9e6ac1e12d4fb
Srujana Addanki <152511711+Srujanaaddanki@users.noreply.github.com>
2026-09-08 23:01:21 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (22.48%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,10 +1,19 @@
+// class Solution {
+//     public int countCommas(int n) {
+//         int count =1;
+//         if(n < 1000) return 0;
+//         for(int i=1000; i<n; i++){
+//             count += 1;
+//         }
+//         return count;
+//     }
+// }
+
+
+
 class Solution {
-    public int countCommas(int n) {
-        int count =1;
+    public int countCommas(int n){
         if(n < 1000) return 0;
-        for(int i=1000; i<n; i++){
-            count += 1;
-        }
-        return count;
+        return (n - 1000)+1;
     }
 }
